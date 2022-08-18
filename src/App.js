2fba9ad517118fef45d1c1538a7d9422ebd2757b
Ing. Benito Enríquez Mora <enriquez.ico@gmail.com>
2022-08-18 07:52:58 -0500

    feature(setup-hooks-components): set up hooks and components imports in file src/App.js.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import "./App.css";
 
-import QuantityPokemon from "./components/QuantityPokemon";
-import BuyPokemon from "./components/BuyPokemon";
+//  import QuantityPokemon from "./components/QuantityPokemon";
+//  import BuyPokemon from "./components/BuyPokemon";
+import BuyPokemon from "./hooks/BuyPokemon";
+import QuantityPokemon from "./hooks/QuantityPokemon";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import SearcherPokemon from "./components/searcher/SearcherPokemon";
+import ResultPokemon from "./components/searcher/ResultPokemon";
 
 function App() {
+  //  console.log("[actual-state]: ", store.getState());
   return (
-    <div className="App container">
-      <div className="row">
-        <div className="col-12">
-          <div className="card mt-5" style={{ maxWidth: "370px" }}>
-            <div className="row no-gutters">
-              <div className="col-4">
-                <img src="pokemon.jpg" alt="pokemon" className="card-img" />
-              </div>
-              <div className="col-8">
-                <div className="card-body">
-                  <QuantityPokemon />
-                  <BuyPokemon />
+    <Provider store={store}>
+      <div className="App container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card mt-5" style={{ maxWidth: "370px" }}>
+              <div className="row no-gutters">
+                <div className="col-4">
+                  <img src="pokemon.jpg" alt="pokemon" className="card-img" />
+                </div>
+                <div className="col-8">
+                  <div className="card-body">
+                    <QuantityPokemon />
+                    <BuyPokemon />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="col-12 mt-4 border-top pt-3">
+            <SearcherPokemon />
+          </div>
+          <div className="col-12">
+            <ResultPokemon />
+          </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
